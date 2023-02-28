2b63095bdceb4baaa177b1630fd3121c1ec13fd1
Oleg Lyukshin <oleg@lyukshin.ru>
2023-02-28 19:11:15 +0400

    homework 25,27,29

---
+++ homework25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int sum = number1;
+
+    for(int i = 1; i < number2; i++)
+    {
+       sum = sum * number1; 
+    }
+
+Console.WriteLine(sum);
---
+++ homework27/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+//МЕТОД, выводит в консоль задачу пользователю и возвращает из консоли введенное значение типа double
+double PromtDouble (string message)
+{
+  Console.WriteLine(message);
+  double result = Convert.ToDouble(Console.ReadLine());
+  return result;
+} 
+
+//МЕТОД, проверяет входные данные (число) на отрицательный показатель, и в соответствии со
+// значениями и совершает оперцию сложения элементов его разрядности
+
+int UserMathPow (double valueSum)
+{ 
+  bool minusSing = false;
+  if (valueSum > -1 && valueSum < 1)
+  {
+    return 0;
+  }
+  if (valueSum <= -1)
+  {
+    minusSing = true;
+    valueSum = Math.Abs(valueSum);
+  }
+  int tempValue = Convert.ToInt32(Math.Round(valueSum, 1));
+  string str = Convert.ToString(tempValue);
+  int [] tempArr = new int [str.Length];
+  int Sum = 0;
+  for (int i = 0; i < str.Length; i++)
+  {
+    Sum += Convert.ToInt32(str[i].ToString());
+  }
+  if (minusSing)
+  {
+    Sum = -Sum;
+  }
+  return Sum;
+}
+
+//Точка входа в основную программу
+double numberSum = PromtDouble("Enter your number, please");
+Console.WriteLine($"Sum of elements your number {numberSum} is {UserMathPow(numberSum)}");
+
+
