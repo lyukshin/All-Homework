a8d583b2a40f3a402095be663794529a86a37c26
Oleg Lyukshin <oleg@lyukshin.ru>
2023-03-10 18:17:03 +0400

    add homework 47,50,52

---
+++ homework50/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+  int[,] matrix = new int[row, column];
+  Random rnd = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(0, 100);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void SearchNumber(int[,] matr, int getNumber)
+{
+  bool get = true;
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      if (getNumber == matr[i, j])
+      {
+        get = false;
+        break;
+      }
+    }
+  }
+  if (get)
+  {
+    System.Console.Write($" {getNumber} отсутствует в массиве ");
+  }
+  else
+  {
+    System.Console.Write($" {getNumber} имеется в массиве ");
+  }
+
+}
+
+void SearchIndex(int[,] matr, int getIndex1, int getIndex2)
+{
+  bool get = true;
+  var num = 0;
+  if (getIndex1 <= matr.GetLength(0) && getIndex2 <=matr.GetLength(1))
+  {
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      if (matr[i, j] == matr[getIndex1, getIndex2])
+      {
+        get = false;
+        num = matr[i, j];
+      }
+    }
+  }
+  if (get)
+  {
+    System.Console.Write(" По заданому адресу ничего нет ");
+  }
+  else
+  {
+    System.Console.WriteLine($" По заданому адресу имеется следующие число: {num} ");
+  }
+  }
+  else
+  {
+    System.Console.Write(" Поисковые индексы выходят за границы массива!");
+  }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+... (28 lines left)
---
+++ homework52/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+  int[,] matrix = new int[row, column];
+  Random rnd = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(0, 10);
+    }
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void PrintStatic(int[,] matrix)
+{
+  double resul = 0;
+  double sumStolb = 0;
+  for (int i = 0; i < matrix.GetLength(1); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+      sumStolb += matrix[j, i];
+    }
+    resul = sumStolb / matrix.GetLength(0);
+    Console.WriteLine($"Средне арифметическое {i + 1} столбца = {resul}");
+    resul = 0;
+    sumStolb = 0;
+  }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+
+PrintStatic(matrix);
